515695c75894124c71b730812dc0c2a29bfdc5ce
Daniel Gorter <danieljgorter@gmail.com>
2020-04-21 17:45:27 -0400

    Implement AssocOptions

--- build_active_record/lib/03_associatable.rb
+++ build_active_record/lib/03_associatable.rb
@@ -10,23 +10,27 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    self.class_name.constantize
   end
 
   def table_name
-    # ...
+    self.class_name.underscore + "s"
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || "#{name}_id".to_sym
+    @primary_key = options[:primary_key] || :id
+    @class_name = options[:class_name] || name.camelcase
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || "#{self_class_name.underscore}_id".to_sym
+    @primary_key = options[:primary_key] || :id
+    @class_name = options[:class_name] || name.singularize.camelcase
   end
 end
 
