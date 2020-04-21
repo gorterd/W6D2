0a42d6fab559b4a9af8c5a3c6b06b8f139cc26b5
Daniel Gorter <danieljgorter@gmail.com>
2020-04-21 17:24:06 -0400

    Implement Searchable Module

--- build_active_record/lib/02_searchable.rb
+++ build_active_record/lib/02_searchable.rb
@@ -3,10 +3,22 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_str = params.keys.map {|attr| "#{attr} = ?"}.join(' AND ')
+    
+    data = DBConnection.execute(<<-SQL, params.values)
+      SELECT
+        #{self.table_name}.*
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_str}
+    SQL
+    
+    self.parse_all(data)
   end
 end
 
 class SQLObject
+  extend Searchable
   # Mixin Searchable here...
 end
