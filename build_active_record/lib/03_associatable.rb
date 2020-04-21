b6ca7280682e21c2fc45bb5089ba3de6f1cecc1c
Daniel Gorter <danieljgorter@gmail.com>
2020-04-21 18:03:10 -0400

    Implement Associatable

--- build_active_record/lib/03_associatable.rb
+++ build_active_record/lib/03_associatable.rb
@@ -22,7 +22,7 @@ class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
     @foreign_key = options[:foreign_key] || "#{name}_id".to_sym
     @primary_key = options[:primary_key] || :id
-    @class_name = options[:class_name] || name.camelcase
+    @class_name = options[:class_name] || name.to_s.camelcase
   end
 end
 
@@ -30,18 +30,30 @@ class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
     @foreign_key = options[:foreign_key] || "#{self_class_name.underscore}_id".to_sym
     @primary_key = options[:primary_key] || :id
-    @class_name = options[:class_name] || name.singularize.camelcase
+    @class_name = options[:class_name] || name.to_s.singularize.camelcase
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+
+    define_method(name) do 
+      target_model = options.model_class
+      foreign_key_val = self.send(options.foreign_key)
+      target_model.find(foreign_key_val)
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self.to_s, options)
+
+    define_method(name) do 
+      target_model = options.model_class
+      primary_key_val = self.send(options.primary_key)
+      target_model.where(options.foreign_key => primary_key_val)
+    end
   end
 
   def assoc_options
@@ -51,4 +63,5 @@ end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
